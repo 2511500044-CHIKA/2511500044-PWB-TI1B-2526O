5c007db81436faa797060f03ddad5bad4f8fc785
chika zayin salsabila <2511500044@mahasiswa.atmaluhur.ac.id>
2025-12-17 21:27:22 -0700

    Tambahkan penampil flash sukses maupun flash error menggunakan session pada read.php

--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -1,9 +1,21 @@
 <?php
+session_start();
 require 'koneksi.php';
+require 'fungsi.php';
 
 $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
 $q   = mysqli_query($conn, $sql);
+if (!$q) {
+    die("Query Error: " . mysqli_error($conn));
+ }
 ?>
+
+>?php
+    echo"<p>Gagal membaca data tamu: " . htmlspecialchars(mysqli_error($conn)) . "</p>";
+}   elseif(mysqli_num_rows($q) === 0) {
+    echo"<p>Belum ada data yang tersimpan.</p>";
+    echo"<p>Data tamu masih kosong.</p>";
+}   else {
 <table border="1" cellpadding="8" cellspacing="0">
     <tr>
         <th>ID</th>
@@ -19,13 +31,12 @@ $q   = mysqli_query($conn, $sql);
     <th>Pesan</th>
     <th>Created At</th>
 </th>
-
+<?php $i = 1; ?>
     <?php while ($row = mysqli_fetch_assoc($q)): ?>
         <tr>
-            <td><?= $row['cid']; ?></td>
-            <td><?= htmlspecialchars($row['cnama']); ?></td>
-            <td><?= htmlspecialchars($row['cemail']); ?></td>
-            <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
-        </tr>
-    <?php endwhile; ?>
+           <td><?= $i++; ?></td>
+           <td><a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a><  
+           <td><?= $row['cid']; ?></td>
+           <td><?= htmlspecialchars($row['cnama']); ?></td>
+           <?php endwhile; ?>
 </table>
--- pertemuan-12/read_inc.php
+++ pertemuan-12/read_inc.php
@@ -1,5 +1,7 @@
 <?php
+session_start();
 require 'koneksi.php';
+require 'fungsi.php';
 
 $fieldconfig = [
     "nama"  => ["label" => "Nama:","suffix" => ""],
